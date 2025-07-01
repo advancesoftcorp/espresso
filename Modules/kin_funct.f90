9ee96a35ce9ec5c577e5ec2cf9eb429f983b71a2
Satomichi Nishihara <nishihara.satomichi@advancesoft.jp>
2025-07-01 14:40:43 +0900

    fix kin_funct

--- Modules/kin_funct.f90
+++ Modules/kin_funct.f90
@@ -18,19 +18,13 @@ MODULE kin_funct
   SAVE
   PRIVATE
   !
-  REAL(DP), PARAMETER :: pi   = 3.1415926535897932384626433832795_DP
-  REAL(DP), PARAMETER :: C_tf = (3.0_DP / 10.0_DP) * ((3.0_DP * pi * pi) ** (2.0_DP / 3.0_DP))
-  REAL(DP), PARAMETER :: C_vw = 1.0_DP / 8.0_DP
-  !
-  REAL(DP), PARAMETER :: rho_threshold = 1.0E-10_DP
-  !
   PUBLIC :: kin_tf
   PUBLIC :: kin_vw
   !
 CONTAINS
   !
   !---------------------------------------------------------------------------
-  SUBROUTINE kin_tf( length, rho, et, vt )
+  SUBROUTINE kin_tf( length, fact, rho, et, vt )
     !-------------------------------------------------------------------------
     !
     ! ... Kinetic energy functional of Thomas-Fermi model.
@@ -44,6 +38,10 @@ CONTAINS
     REAL(DP), INTENT(INOUT) :: et (length)  ! energy density  (added in place)
     REAL(DP), INTENT(INOUT) :: vt (length)  ! potential       (added in place)
     !
+    REAL(DP), PARAMETER :: pi   = 3.1415926535897932384626433832795_DP
+    REAL(DP), PARAMETER :: C_tf = (3.0_DP / 10.0_DP) * ((3.0_DP * pi * pi) ** (2.0_DP / 3.0_DP))
+    REAL(DP), PARAMETER :: rho_threshold = 1.0E-10_DP
+    !
     INTEGER  :: ir
     REAL(DP) :: r, r23
     !
@@ -56,9 +54,9 @@ CONTAINS
           !
           r23 = r ** (2.0_DP / 3.0_DP)
           !
-          et(i) = et(i) + fact * C_tf * r * r23
+          et(ir) = et(ir) + fact * C_tf * r * r23
           !
-          vt(i) = vt(i) + fact * (5.0_DP / 3.0_DP) * C_tf * r23
+          vt(ir) = vt(ir) + fact * (5.0_DP / 3.0_DP) * C_tf * r23
           !
        END IF
        !
@@ -68,7 +66,7 @@ CONTAINS
   END SUBROUTINE kin_tf
   !
   !---------------------------------------------------------------------------
-    SUBROUTINE kin_vw( length, fact, rho, grho, et, v1t, v2t )
+  SUBROUTINE kin_vw( length, fact, rho, grho, et, v1t, v2t )
     !-------------------------------------------------------------------------
     !
     ! ... Kinetic energy functional of von Weizsacker model.
@@ -76,30 +74,34 @@ CONTAINS
     !
     IMPLICIT NONE
     !
-    INTEGER,  INTENT(IN)    :: length       ! array length
-    REAL(DP), INTENT(IN)    :: fact         ! scaling factor
-    REAL(DP), INTENT(IN)    :: rho(length)  ! charge density
-    REAL(DP), INTENT(IN)    :: grho(:)      ! gradient of rho: |\nabla rho|^2
-    REAL(DP), INTENT(INOUT) :: et (length)  ! energy density             (added in place)
-    REAL(DP), INTENT(INOUT) :: v1t(length)  ! potential of density part  (added in place)
-    REAL(DP), INTENT(INOUT) :: v2t(length)  ! potential of gradient part (added in place)
+    INTEGER,  INTENT(IN)    :: length        ! array length
+    REAL(DP), INTENT(IN)    :: fact          ! scaling factor
+    REAL(DP), INTENT(IN)    :: rho (length)  ! charge density
+    REAL(DP), INTENT(IN)    :: grho(length)  ! gradient of rho: |\nabla rho|^2
+    REAL(DP), INTENT(INOUT) :: et  (length)  ! energy density             (added in place)
+    REAL(DP), INTENT(INOUT) :: v1t (length)  ! potential of density part  (added in place)
+    REAL(DP), INTENT(INOUT) :: v2t (length)  ! potential of gradient part (added in place)
+    !
+    REAL(DP), PARAMETER :: C_vw = 1.0_DP / 8.0_DP
+    REAL(DP), PARAMETER :: rho_threshold  = 1.0E-6_DP
+    REAL(DP), PARAMETER :: grho_threshold = 1.0E-10_DP
     !
     INTEGER  :: ir
-    REAL(DP) :: r, gr2
+    REAL(DP) :: r, gr
     !
-!$omp parallel do default(shared) private(ir, r, gr2)
+!$omp parallel do default(shared) private(ir, r, gr)
     DO ir = 1, length
        !
-       r   = rho(i)
-       gr2 = grho(i)
+       r  = rho(ri)
+       gr = grho(ri)
        !
-       IF ( r > rho_threshold ) THEN
+       IF ( r > rho_threshold .AND. gr > grho_threshold ) THEN
           !
-          et(i)  = et(i)  + fact * C_vw * gr2 / r
+          et(ir)  = et(ir)  + fact * C_vw * gr / r
           !
-          v1t(i) = v1t(i) - fact * C_vw * gr2 / (r * r)
+          v1t(ir) = v1t(ir) - fact * C_vw * gr / (r * r)
           !
-          v2t(i) = v2t(i) + fact * C_vw / r
+          v2t(ir) = v2t(ir) + fact * 2.0_DP * C_vw / r
           !
        END IF
        !
